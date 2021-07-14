e8b468763bbb587a00cfefd0e5c173a37ada2eda
romain <romain.perret@ulb.be>
2021-07-14 14:09:35 +0200

    basic single page login and home screen.

--- src/App.js
+++ src/App.js
@@ -4,18 +4,61 @@ import Login from "./Login";
 import FileExplorer from "./FileExplorer";
 import React, {useState} from "react";
 
-function App() {
-  let [loggedIn, setLoggedIn] = useState(false);
-  let [webId, setWebId] = useState("");
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory
+} from "react-router-dom";
 
-  console.log("App webId: "+ webId);
-  return (
-    <div className="app-div">
-      {webId !== "" && loggedIn ? <div><h1> You are logged in !!!</h1><h3>webID: {webId}</h3> <FileExplorer webId={webId}/> </div> :
-      <Login setLoggedIn={setLoggedIn} setWebId={setWebId}/>
-      }
-    </div>
-  );
+// import { createBrowserHistory } from 'history';
+
+function App() {
+    let [loggedIn, setLoggedIn] = useState(false);
+    let [webId, setWebId] = useState("");
+    // let history = useHistory();
+
+    function isLoggedIn()
+    {
+        return (webId !== "") && loggedIn;
+    }
+
+    return (
+        <Router>
+          <div className="app-div">
+            <Switch>
+                <Route path="/login">
+                    <Login setWebId={setWebId} setLoggedIn={setLoggedIn}/>
+                    {/* We execute this separately and after the login, this allows
+                    an automatic redirect to '/home' when we come back from login form. */}
+                    {isLoggedIn() ? <Redirect to="/"/> : null} 
+                </Route>
+                <Route path="/home">
+                    {isLoggedIn() ? <Home webId={webId}/> : <Redirect to="/"/>}
+                </Route>
+                <Route path="/">
+                    {isLoggedIn() ? <Redirect to="/home"/> : <Redirect to="/login"/>}
+                </Route>
+            </Switch>
+          </div>
+        </Router>
+    );
 }
+
+function Home(props)
+{
+    let webId = props.webId;
+
+    return (
+        <div>
+            <h1>You are logged in !!!</h1>
+            <h3>webID: {webId}</h3>
+            <FileExplorer webId={webId}/> 
+        </div>
+    );
+}
+
 
 export default App;
