3b75991699799f2d15adda0c2b5408c0e6df3c3e
Kakooza Jerry <abrahamkakooza@gmail.com>
2021-07-15 12:17:12 +0300

    Updates

--- static/js/4.17c1a368.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpreclaim_your_data=this.webpackJsonpreclaim_your_data||[]).push([[4],{135:function(a,c){},137:function(a,c){}}]);
-//# sourceMappingURL=4.17c1a368.chunk.js.map
---
+++ static/js/4.40c70edb.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreclaim_your_data=this.webpackJsonpreclaim_your_data||[]).push([[4],{161:function(a,c){},163:function(a,c){}}]);
+//# sourceMappingURL=4.40c70edb.chunk.js.map
