fb619f1408f20d14bffa852ad2e6fb88d8bb3344
Xavier Bertels <github@xavierbertels.com>
2021-07-20 17:02:03 +0200

    Updates

--- static/js/4.17c5fbc3.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpreclaim_your_data=this.webpackJsonpreclaim_your_data||[]).push([[4],{243:function(a,c){},245:function(a,c){}}]);
-//# sourceMappingURL=4.17c5fbc3.chunk.js.map
---
+++ static/js/4.199fe7fd.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreclaim_your_data=this.webpackJsonpreclaim_your_data||[]).push([[4],{244:function(a,c){},246:function(a,c){}}]);
+//# sourceMappingURL=4.199fe7fd.chunk.js.map
