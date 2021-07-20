46b9f97bedffd5e57cf4bfd22cf47a1de91b603b
Xavier Bertels <github@xavierbertels.com>
2021-07-20 16:41:42 +0200

    fix(AppBar): position fixed, add padding to container

--- src/App.js
+++ src/App.js
@@ -142,7 +142,7 @@ function MenuBar(props)
     let gotoScreen = props.gotoScreen;
 
     return(
-        <AppBar position="sticky" className={classes.appBar}>
+        <AppBar position="fixed" className={classes.appBar}>
             <Toolbar>
                 <Typography variant="h6" color="inherit">
                     WePod
