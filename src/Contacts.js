699b89478f4e3a0b77807aa831ca897da7917d40
romain <romain.perret@ulb.be>
2021-07-22 16:13:21 +0200

    Working contacts query.

--- src/Contacts.js
+++ src/Contacts.js
@@ -2,15 +2,22 @@
 
 import { useState,React, useEffect } from "react";
 import {executeQuery, printBindings} from "./rdf";
+import "./Contacts.css";
 import {
     useSession,
   } from "@inrupt/solid-ui-react";
 
+import PersonIcon from '@material-ui/icons/Person';
 
-const getPersonFilesQuery = "SELECT * WHERE { ?s http://www.w3.org/2006/vcard/ns#fn ?o }";
 
-const contactsWebIdSource = "https://pod.inrupt.com/wepodrom/contacts/Person/eafe09c7-4741-4251-91e3-90b0ea2aa043/index.ttl#this";
-const contactsWebIdQuery= "SELECT ?o WHERE { ?s <http://www.w3.org/2006/vcard/ns#value> ?o }";
+
+// const getPersonFilesQuery = "SELECT * WHERE { ?s http://www.w3.org/2006/vcard/ns#fn ?o }";
+
+const getContactsBasicInfoSource = "https://pod.inrupt.com/wepodrom/contacts/people.ttl#this";
+const getContactsBasicInfoQuery = "SELECT ?s ?o WHERE {?s <http://www.w3.org/2006/vcard/ns#fn> ?o}";
+
+// const contactsWebIdSource = "https://pod.inrupt.com/wepodrom/contacts/Person/eafe09c7-4741-4251-91e3-90b0ea2aa043/index.ttl#this";
+// const contactsWebIdQuery= "";
 
 const defaultSource= "https://pod.inrupt.com/wepodrom/profile/card#me";
 const defaultQuery = "SELECT * WHERE {?s ?p ?o}";
@@ -18,13 +25,14 @@ const defaultQuery = "SELECT * WHERE {?s ?p ?o}";
 function Contacts(props)
 {
 	let webId = props.webId;
+
 	const { session } = useSession();
 	const [bindings, setBindings] = useState([]);
 
-	
+
 	async function fetchContacts(){
 		console.log(`fetching contacts of webid:'${webId}'`);
-		let resBindings = await executeQuery(contactsWebIdQuery,[contactsWebIdSource], session);
+		let resBindings = await executeQuery(getContactsBasicInfoQuery,[getContactsBasicInfoSource], session);
 		// let contactWebIds = await executeQuery();
 		await setBindings(resBindings);
 	}
@@ -48,13 +56,33 @@ function Contacts(props)
 	      </>
 	    );
 	  }
+
+  	function Contact(props)
+  	{
+  		let binding = props.binding;
+  		const boundVariables = binding['_root'].entries.map(e => e[0]);
+
+  		let username = binding['_root'].entries[1][1]['id'];
+  		// username = username.replace("/(\"|\"$)/", '');
+  		console.log("username:", username)
+  		let turtleFileUrl = binding['_root'].entries[0][1]['id'];
+  		// turtleFileUrl = turtleFileUrl.replace("/(\"|\"$)/", '');
+
+  		return (
+		      	<div className="contact">
+		      		<PersonIcon/>
+		          	<p>{username}:</p>
+		          	<p>{turtleFileUrl}</p>
+		        </div>
+  		);
+  	}
 	
 	return (
 		<div>
           {bindings.map(binding => {
-          		console.log("Binding:", binding);return bindingsView(binding)
+          		console.log("Binding:", binding); return (<Contact binding={binding}/>)
           	}) }
-      </div>
+      	</div>
 	);
 }
 
