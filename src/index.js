9fdf6fb9d1dc7fb33cde9f4a896a86fe93586d72
cyrille.mathieu <cyrille.mathieu@student.howest.be>
2021-07-26 14:27:57 +0200

    Remove unused modules, commented out code

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        {/*basename={process.env.PUBLIC_URL}*/}
-        <HashRouter> 
+        <HashRouter>
             <App/>
         </HashRouter>
     </React.StrictMode>,
