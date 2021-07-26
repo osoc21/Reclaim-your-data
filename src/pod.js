a8ea807dd293d27abab56878d03ced7803b91232
cyrille.mathieu <cyrille.mathieu@student.howest.be>
2021-07-26 14:29:59 +0200

    Remove unused variables

--- src/pod.js
+++ src/pod.js
@@ -1,11 +1,6 @@
-
-
-const solidwebPattern = "https:\/\/(\w+\.)solidweb.org\/";
-const podInruptPattern = "https:\/\/pod\.inrupt\.com\/\w+\/";
 const tempPodPattern = /https:\/\/(\w+\.)solidweb.org\/|https:\/\/pod\.inrupt\.com\/\w+\//;
 
-function getPODUrlFromWebId(webId) 
-{
+function getPODUrlFromWebId(webId) {
     return webId.match(tempPodPattern)[0];
 }
 
